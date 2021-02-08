57515727f49cf45b73e43e8274783b52c6fb6094
Ali Kesserwani <alikesserwani23@gmail.com>
2021-02-08 13:22:39 -0600

    Update play.js

--- play.js
+++ play.js
@@ -1,8 +1,34 @@
 //Play.js
 
+$(document).ready(function(){
+    $("#btn").click(function(){
 
+        var form_data = new FormData();
+        var imageOne = $('#image_one')[0].files;
+        var imageTwo = $('#image_two')[0].files;
+        var imageThree = $('#image_three')[0].files;
 
-function upload_image(){
-    console.log("Hello")
-}
+        if(imageOne.length > 0 && imageTwo.length > 0 && imageThree.length > 0 ){
+           form_data.append('file',imageOne[0]);
+           form_data.append('file',imageTwo[0]);
+           form_data.append('file',imageThree[0]);
 
+           $.ajax({
+              url: 'updateGame.php',
+              type: 'post',
+              data: form_data,
+              contentType: false,
+              processData: false,
+              success: function(response){
+                 if(response != 0){
+                    console.log("Succesfully Uploaded")
+                }else{
+                    alert('file not uploaded');
+                 }
+              },
+           });
+        }else{
+           alert("Please select a file.");
+        }
+    });
+});
