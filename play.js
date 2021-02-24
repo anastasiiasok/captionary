dd60c2a9faa03173f49e42840a07f704d1323e0c
Ali Kesserwani <alikesserwani@Alis-MacBook-Pro.local>
2021-02-24 15:26:23 -0600

    Ajax

--- play.js
+++ play.js
@@ -1,34 +1,3 @@
-// $(document).ready(function () {
-//   $("#btn").click(function () {
-//     var form_data = new FormData();
-//     var imageOne = $("#image_one")[0].files;
-//     var imageTwo = $("#image_two")[0].files;
-//     var imageThree = $("#image_three")[0].files;
-
-//     if (imageOne.length > 0 && imageTwo.length > 0 && imageThree.length > 0) {
-//       form_data.append("file", imageOne[0]);
-//       form_data.append("file", imageTwo[0]);
-//       form_data.append("file", imageThree[0]);
-
-//       $.ajax({
-//         url: "updateGame.php",
-//         type: "post",
-//         data: form_data,
-//         contentType: false,
-//         processData: false,
-//         success: function (response) {
-//           if (response != 0) {
-//             console.log("Succesfully Uploaded");
-//           } else {
-//             alert("file not uploaded");
-//           }
-//         },
-//       });
-//     } else {
-//       alert("Please select a file.");
-//     }
-//   });
-// });
 
 const form = document.querySelector("#form");
 const fileInput = document.querySelector("#image_one");
@@ -73,3 +42,38 @@ function generatePrevievImages(files) {
     previewFunc(file, previewImages);
   }
 }
+
+
+
+//submit button
+//upload images to ajax
+
+ $(document).ready(function () {
+   $("#submit_btn").click(function () {
+    var form_data = new FormData();
+    var blob = $("#image_one").files;
+
+    if (blob.length > 0) {
+       form_data.append("file", blob);
+
+       $.ajax({
+         url: "updateGame.php",
+         type: "post",
+          data: form_data,
+          contentType: false,
+          processData: false,
+          success: function (response) {
+            if (response != 0) {
+              alert("Succesfully Uploaded");
+            } else {
+              alert("file not uploaded");
+            }
+          },
+        });
+      } else {
+        alert("Please select a file.");
+      }
+    });
+  });
+
+  
