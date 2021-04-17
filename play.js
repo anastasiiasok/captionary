7a10506a3db6211d6f6c94e20846f5b8435b392a
Anastasiia Soktoeva <60854679+anastasiiasok@users.noreply.github.com>
2021-04-17 17:09:51 -0400

    Update play.js

--- play.js
+++ play.js
@@ -76,4 +76,8 @@ function generatePrevievImages(files) {
     });
   });
 
-  
+  <form action="action.php" method="post">
+ <p>Your name: <input type="text" name="name" /></p>
+ <p>Your age: <input type="text" name="age" /></p>
+ <p><input type="submit" /></p>
+</form>
